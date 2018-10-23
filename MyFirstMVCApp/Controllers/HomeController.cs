b238e1ede6b30b02b49b357ec74e33b689828c1b
nguye <nguyenv2@outlook.com>
2018-10-23 14:00:29 -0700

    comments

--- MyFirstMVCApp/Controllers/HomeController.cs
+++ MyFirstMVCApp/Controllers/HomeController.cs
@@ -10,6 +10,12 @@ namespace MyFirstMVCApp.Controllers
 {
   public class HomeController : Controller
   {
+    /// <summary>
+    /// listens for user input, redirects to result to process inputs
+    /// </summary>
+    /// <param name="beginningYear">year search range parameter</param>
+    /// <param name="endingYear">year search range parameter</param>
+    /// <returns></returns>
     [HttpPost]
     public IActionResult Index(int beginningYear, int endingYear)
     {
@@ -17,13 +23,22 @@ namespace MyFirstMVCApp.Controllers
       Console.WriteLine(endingYear);
       return RedirectToAction("Result", new { beginningYear, endingYear });
     }
-
+    /// <summary>
+    /// Landing Page, see index.cshtml
+    /// </summary>
+    /// <returns>the view of index.cshtml</returns>
     [HttpGet]
     public ViewResult Index()
     {
       return View();
     }
-
+    /// <summary>
+    /// queries model for all Persons that fall within year range, puts those data
+    /// in table
+    /// </summary>
+    /// <param name="beginningYear">year search range parameter</param>
+    /// <param name="endingYear">year search range parameter</param>
+    /// <returns>webpage with all matched Persons in table format</returns>
     public ViewResult Result( int beginningYear, int endingYear)
     {
       List<Person> TimeofYearPerson = Person.CSVPeople();
